aca1d1cb095c05e14ab10ccd821cdfba8f4133aa
skyzorg <colin@xaxxon.com>
2013-11-04 09:51:38 -0800

    add dev files

---
+++ WEB-INF/src/developer/Scratch.java
@@ -0,0 +1,16 @@
+package developer;
+
+public class Scratch {
+
+	public static void main(String[] args) {
+//		String str = "95K";
+//		int n = Integer.parseInt(str.replaceAll("\\D", ""));
+//		System.out.println(n);
+		
+//		int n = (int) ( (double) -10 * Math.pow(50.0/255.0, 1.0) );
+		int n = (int) ( (double) -10 *50/255);
+		System.out.println(n);
+
+	}
+
+}
---
+++ WEB-INF/src/oculusPrime/ScriptRunner.java
@@ -0,0 +1,18 @@
+package oculusPrime;
+
+/*
+ * function to auto run all scripts under /oculusPrime/webapps/scripts/startup
+ * function to return filenames of all startup scripts
+ * function to run particular startup script
+ * function to return all available normal scripts filenames
+ * function to run particular normal script filenames
+ * 
+ * run each script with args: host, username (user0), password (hashed pass0), port
+ * support for python, ruby, others?
+ * choose language by filename extension
+ * manual settings for linux/windows language inerpreter locations
+ * 
+ */
+public class ScriptRunner {
+	
+}
