d57117686fcdaca9e1307a6b0ff33843a50e1dc8
bradz <brad.zdanivsky@gmail.com>
2015-04-15 16:28:44 -0700

    linux time reboot fixed

--- WEB-INF/src/oculusPrime/SystemWatchdog.java
+++ WEB-INF/src/oculusPrime/SystemWatchdog.java
@@ -40,15 +40,16 @@ public class SystemWatchdog {
 	private class Task extends TimerTask {
 		public void run() {
 	
-			// regular reboot based on linux boot time
-			if ((state.getLong(values.linuxboot) > STALE) 
+			// regular reboot based on Linux boot time
+			if((System.currentTimeMillis() - state.getLong(values.linuxboot) > STALE) 
 					&& state.get(State.values.dockstatus).equals(AutoDock.DOCKED)){
 				
 				String boot = new Date(state.getLong(values.linuxboot)).toString();				
 				Util.log("rebooting, last boot was: " + boot, this);
-				application.driverCallServer(PlayerCommands.reboot, null);
+			 	application.driverCallServer(PlayerCommands.reboot, null);
 			}
-						
+			
+			
 			// safety: check for force_undock command from battery firmware
 			if (state.getBoolean(State.values.forceundock) && state.get(State.values.dockstatus).equals(AutoDock.DOCKED)) {
 				Util.log("System WatchDog, force undock", this);
